3d1ea4bdb734640210243bd526b48722cd7e398b
wizard-jS <spraguej624@gmail.com>
2019-04-04 16:30:46 -0700

    Initialize a redux store for the server

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = serverStore();
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3002, () => {
