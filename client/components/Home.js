7d659e6427460fe814b4c0b0267c8a53691612c2
wizard-jS <spraguej624@gmail.com>
2019-04-03 19:31:19 -0700

    Initialize Home component

---
+++ client/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Im the very BEST home page!</div>;
+      <button onClick={() => console.log("Button was clicked!")}>
+        Click Me!
+      </button>
+      ;
+    </div>
+  );
+};
+
+export default Home;
