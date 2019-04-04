cdaa2ca590139436fd3d44ea1a0c533cbc00d73e
wizard-jS <spraguej624@gmail.com>
2019-04-04 16:30:28 -0700

    Fix Type

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Home from "./client/components/Home";
+import Home from "./components/Home";
 
 export default () => {
   return (
