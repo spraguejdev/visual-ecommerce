b90eaf82e2f09f61e2c01b393c2c785ee294e617
wizard-jS <spraguej624@gmail.com>
2019-04-04 16:32:19 -0700

    Initialize a redux store and provide for the server codebase

--- server/src/client/helpers/renderer.js
+++ server/src/client/helpers/renderer.js
@@ -2,12 +2,16 @@ import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import Routes from "../Routes";
+import { Provider } from "react-redux";
+import store from "./serverStore";
 
-export default req => {
+export default (req, store) => {
   const content = renderToString(
-    <StaticRouter location={req.path} context={{}}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
 
   return `
