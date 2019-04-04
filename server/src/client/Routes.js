32bdd87296c8a70372e467d1bb9419d3ff44b817
wizard-jS <spraguej624@gmail.com>
2019-04-04 15:33:38 -0700

    Refactor File Structure

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./client/components/Home";
 
 export default () => {
   return (
--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,15 +1,12 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
-const webpackNodeExternal = require("webpack-node-externals");
 const config = {
-  target: "node",
-  entry: "./server/index.js",
+  entry: "./src/client/App.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
-  },
-  externals: [webpackNodeExternal()]
+    path: path.resolve(__dirname, "public")
+  }
 };
 
 module.exports = merge(baseConfig, config);
