303d587cc29bc35fac6ee85d2b805c9b509b7412
wizard-jS <spraguej624@gmail.com>
2019-04-04 16:31:18 -0700

    Fix typo for the reducers

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import reducers from "./reducers/combineReducers";
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
