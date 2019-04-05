dd46a5b905754de05cbbfe1a636fbc011072f480
wizard-jS <spraguej624@gmail.com>
2019-04-04 17:33:44 -0700

    Implement a users component

---
+++ server/src/client/components/UsersList.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../actions/index";
+
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        Here is a big list of users
+        <ul>
+          {this.props.users.map(user => {
+            return <li key={user.id}>{user.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchUsers }
+)(UsersList);
