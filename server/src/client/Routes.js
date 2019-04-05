3757cf177c5dc2dd06eda03ab42d98d2f4f36d82
wizard-jS <spraguej624@gmail.com>
2019-04-04 18:35:13 -0700

    Add product route

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
+import AdminPage from "./components/AdminPage";
 
 export default () => {
   return (
     <div>
       <Route exact path="/" component={Home} />
       <Route exact path="/users" component={UsersList} />
+      <Route exact path="/products" component={AdminPage} />
     </div>
   );
 };
