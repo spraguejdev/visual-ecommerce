09803b7f4c2131c837130a8f00a1dcaf9e29bdff
wizard-jS <spraguej624@gmail.com>
2019-04-04 20:29:41 -0700

    Implement loading data into the redux store and then rendering app

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,22 @@ import "babel-polyfill";
 import express from "express";
 import renderer from "./client/helpers/renderer";
 import serverStore from "./client/helpers/serverStore";
-import { matchRoutes } from 'react-router-config';
-import Routes from './client/Routes':
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 
 const app = express();
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = serverStore();
-  
-  matchRoutes(Routes, req.path)
-  
-  res.send(renderer(req, store));
+
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3002, () => {
