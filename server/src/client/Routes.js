d1ebdca2e622aeac58e7d72cebce1b97895cc353
wizard-jS <spraguej624@gmail.com>
2019-04-04 19:57:37 -0700

    Refactor routes to use the package react-router-config

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 import AdminPage from "./components/AdminPage";
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/users" component={UsersList} />
-      <Route exact path="/products" component={AdminPage} />
-    </div>
-  );
-};
+export default [
+  {
+    path: "/",
+    component: Home,
+    exact: true
+  },
+  {
+    path: "/users",
+    component: UsersList,
+    exact: true
+  },
+  {
+    path: "/products",
+    component: AdminPage,
+    exact: true
+  }
+];
