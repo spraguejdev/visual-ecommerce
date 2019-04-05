f3061728655bc9efdbca164a353af4e49becbc27
wizard-jS <spraguej624@gmail.com>
2019-04-04 19:58:03 -0700

    Refactor to use the matchRoutes method from react-router-config

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,17 @@ import "babel-polyfill";
 import express from "express";
 import renderer from "./client/helpers/renderer";
 import serverStore from "./client/helpers/serverStore";
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes':
 
 const app = express();
 
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = serverStore();
-
+  
+  matchRoutes(Routes, req.path)
+  
   res.send(renderer(req, store));
 });
 
