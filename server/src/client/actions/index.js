8cdf18fbd20ad1a71ab527253447df345d86f36a
wizard-jS <spraguej624@gmail.com>
2019-04-04 18:35:44 -0700

    Add action creator to fetch a list of products from the api

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
 export const FETCH_USERS = "fetch_users";
+export const FETCH_PRODUCTS = "fetch_products";
 export const fetchUsers = () => async dispatch => {
   const res = await axios.get("http://react-ssr-api.herokuapp.com/users");
 
   dispatch({
     type: FETCH_USERS,
+    payload: res
+  });
+};
+
+export const fetchProducts = () => async dispatch => {
+  const res = await axios.get("http://localhost:3003/products");
+  dispatch({
+    type: FETCH_PRODUCTS,
     payload: res
   });
 };
