f46f7ae8f03b328d056ac51e9eb14bda2b4df670
wizard-jS <spraguej624@gmail.com>
2019-04-06 22:22:49 -0700

    Initialize app component (header)

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -1,33 +1,16 @@
-import "babel-polyfill";
 import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import reducers from "./reducers/combineReducers";
 import { renderRoutes } from "react-router-config";
-import axios from "axios";
+import Header from "./components/Header";
 
-// Custom instance of axios
-const axiosInstance = axios.create({
-  // This will prepend to the endpoint when this function is called.
-  baseURL: "/api"
-});
+const App = ({ route }) => {
+  return (
+    <div>
+      <Header />
+      {renderRoutes(route.routes)}
+    </div>
+  );
+};
 
-const store = createStore(
-  reducers,
-  window.INITIAL_STATE,
-  // The the axios instance to all of our action creators
-  applyMiddleware(thunk.withExtraArgument(axiosInstance))
-);
-
-ReactDOM.hydrate(
-  <Provider store={store}>
-    <BrowserRouter>
-      <div>{renderRoutes(Routes)}</div>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+export default {
+  component: App
+};
