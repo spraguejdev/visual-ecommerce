34912d01836f5af2aa07af25f3a56e8c6f7038e6
wizard-jS <spraguej624@gmail.com>
2019-04-06 21:49:40 -0700

    Initialize a proxy in our server

--- server/src/index.js
+++ server/src/index.js
@@ -4,12 +4,26 @@ import renderer from "./client/helpers/renderer";
 import serverStore from "./client/helpers/serverStore";
 import { matchRoutes } from "react-router-config";
 import Routes from "./client/Routes";
+import proxy from "express-http-proxy";
 
 const app = express();
 
+// Proxy for api, any route trying to access this route will be sent off to the api
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.headers["x-forwarded-hots"] = "localhost:3000";
+      return opts;
+    }
+  })
+);
+
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-  const store = serverStore();
+  // Create our server Store here
+  // Pass in our req objet to the createStore function to we can use the cookie
+  const store = serverStore(req);
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
