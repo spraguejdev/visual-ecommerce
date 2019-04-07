db29549cd2971de09f5c612425c9f52a0cf094b9
wizard-jS <spraguej624@gmail.com>
2019-04-06 22:23:53 -0700

    Add our App component and nest all other routes

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -2,21 +2,28 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import UsersListPage from "./pages/UsersListPage";
 import AdminPage from "./pages/AdminPage";
+import App from "./App";
 
+// App is the component that will be shown on all pages
 export default [
   {
-    path: "/",
-    ...HomePage,
-    exact: true
-  },
-  {
-    ...UsersListPage,
-    path: "/users",
-    exact: true
-  },
-  {
-    ...AdminPage,
-    path: "/products",
-    exact: true
+    ...App,
+    routes: [
+      {
+        path: "/",
+        ...HomePage,
+        exact: true
+      },
+      {
+        ...UsersListPage,
+        path: "/users",
+        exact: true
+      },
+      {
+        ...AdminPage,
+        path: "/products",
+        exact: true
+      }
+    ]
   }
 ];
