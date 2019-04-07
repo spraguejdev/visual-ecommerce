748fdc3f37e899e1c15225096bec91e1732b19b3
wizard-jS <spraguej624@gmail.com>
2019-04-07 10:06:49 -0700

    Add action creator to fetch current authenicated user

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -2,19 +2,30 @@ import axios from "axios";
 
 export const FETCH_USERS = "fetch_users";
 export const FETCH_PRODUCTS = "fetch_products";
+export const FETCH_CURRENT_USER = "fetch_current_user";
+
+// When redux thunk calls the function, (atuomaticall invoke), it will now have two extra arguments.
 export const fetchUsers = () => async (dispatch, getState, api) => {
   // We no longer have to use the entire endpoint
   const res = await api.get("/users");
 
+  // Once we get a response back from the api, we dispatch the FETCH_USERS action
   dispatch({
     type: FETCH_USERS,
     payload: res
   });
 };
 
-// When redux thunk calls the function, (atuomaticall invoke), it will now have two extra arguments.
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get("/current_user");
+
+  dispatch({
+    type: FETCH_CURRENT_USER,
+    payload: res
+  });
+};
+
 export const fetchProducts = () => async dispatch => {
-  // rather than using the base axio library, we use the custom instance
   const res = await axios.get("http://localhost:3003/products");
   dispatch({
     type: FETCH_PRODUCTS,
