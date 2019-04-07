ffc16ab7fad43469da726b98f51478c50c34bdea
wizard-jS <spraguej624@gmail.com>
2019-04-06 21:51:13 -0700

    Refactor endpoint and use the custom axios api

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const FETCH_USERS = "fetch_users";
 export const FETCH_PRODUCTS = "fetch_products";
-export const fetchUsers = () => async dispatch => {
-  const res = await axios.get("http://react-ssr-api.herokuapp.com/users");
+export const fetchUsers = () => async (dispatch, getState, api) => {
+  // We no longer have to use the entire endpoint
+  const res = await api.get("/users");
 
   dispatch({
     type: FETCH_USERS,
@@ -11,7 +12,9 @@ export const fetchUsers = () => async dispatch => {
   });
 };
 
+// When redux thunk calls the function, (atuomaticall invoke), it will now have two extra arguments.
 export const fetchProducts = () => async dispatch => {
+  // rather than using the base axio library, we use the custom instance
   const res = await axios.get("http://localhost:3003/products");
   dispatch({
     type: FETCH_PRODUCTS,
