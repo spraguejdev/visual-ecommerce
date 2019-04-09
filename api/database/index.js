c9412a1a9ef63ddcffa805b57cd66ec684061671
wizard-jS <spraguej624@gmail.com>
2019-04-09 13:27:54 -0700

    Change mongodb url

--- api/database/index.js
+++ api/database/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
-const db = mongoose.connect("mongodb://localhost/items", {
-  useNewUrlParse: true
-});
+const db = mongoose.connect(
+  "mongodb+srv://john:zillowtalk@zillow-talk-db-ujzgi.mongodb.net/test?retryWrites=true"
+);
 
 db.then(db => console.log("Connect to mongoose")).catch(err => {
   console.log("there was a problem connecto to mongo");
