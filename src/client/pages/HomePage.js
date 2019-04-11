ed2f5bc19c588985d299abdfb48098fcb8f9106e
wizard-jS <spraguej624@gmail.com>
2019-04-10 23:19:25 -0700

    Add unordered list of technologies used to this point

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -6,7 +6,18 @@ const Home = () => {
       <div className="container">
         <h1 className="display-4 text-center">Welcome!</h1>
         <p className="lead text-center">
-          Checkout these awesome Server Side Rendering Features
+          Checkout these awesome Server Side Rendering Features!
+          <p>Built with:</p>
+          <ul>
+            <li>Javascript ES6</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>React Router</li>
+            <li>Express</li>
+            <li>Node.js</li>
+            <li>Mongodb</li>
+            <li>OAuth Authenication</li>
+          </ul>
         </p>
       </div>
     </div>
