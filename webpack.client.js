c463d59983006e220e50747c3140fce4ef7678c5
wizard-jS <spraguej624@gmail.com>
2019-04-10 23:19:33 -0700

    Housekeeping

--- webpack.client.js
+++ webpack.client.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
+
 const config = {
   entry: "./src/client/App.js",
   output: {
